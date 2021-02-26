9b28bbc7fd4388fbd3b1ba1b94355ea4908a96e0
Justin Hill <jrhill426@gmail.com>
2021-02-26 15:36:58 -0700

    The good ol working tree

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// document.getElementById("getCategory").addEventListener("click", function(event) {
+//   event.preventDefault();
+//   const value = document.getElementById("input").value;
+//
+//   //check if field is empty
+//   if (value === "") {
+//     return;
+//   }
+//   console.log(value);
+//
 //   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 //   fetch(url)
 //     .then(function(response) {
@@ -13,26 +23,25 @@
 //       // updateResult(json.text);
 //     });
 
+document.getElementById("getRandom").addEventListener("click", function(event){
+  event.preventDefault();
+  const url2 = "https://www.themealdb.com/api/json/v1/1/random.php";
+  fetch(url2)
+    .then(function(response) {
+      //make sure request was successful
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Taste Dive API service: " + response.statusText
+        }
+      }
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      let results;
+      // updateResult(json.text);
+    });
+})
 
-
-// document.getElementById("getRandom").addEventListener("click", function(event){
-//   event.preventDefault();
-//   const url2 = "https://www.themealdb.com/api/json/v1/1/random.php";
-//   fetch(url2)
-//     .then(function(response) {
-//       //make sure request was successful
-//       if (response.status != 200) {
-//         return {
-//           text: "Error calling the Taste Dive API service: " + response.statusText
-//         }
-//       }
-//       return response.json();
-//     }).then(function(json) {
-//       console.log(json);
-//       let results;
-//       // updateResult(json.text);
-//     });
-// })
 document.getElementById("categorySubmit").addEventListener("click", function(event){
   event.preventDefault();
   const category = document.getElementById("categoryInput").value;
@@ -51,7 +60,7 @@ document.getElementById("categorySubmit").addEventListener("click", function(eve
       let results;
       results += "<h1> " + category + "Meals </h1>";
       results += "<section class = 'meal-grid'>"
-      
+
       for(let i = 0; i < json.meals.length; i++) {
         results += "<div class = 'mealResults'>";
         results += "<p> " + json.meals[i].strMeal + "</p>";
@@ -59,13 +68,11 @@ document.getElementById("categorySubmit").addEventListener("click", function(eve
         results += "</div>";
       }
       results += "</section>"
-      
-      
+
+
       updateResult(results);
     });
 })
-
-
 
 function updateResult(info) {
   document.getElementById('results').innerHTML = info;
