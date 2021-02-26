6db8b8ee929c3051f2453b551af41f030f99b203
Justin Hill <jrhill426@gmail.com>
2021-02-26 15:18:03 -0700

    Added in a meal results section

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
-document.getElementById("getCategory").addEventListener("click", function(event) {
+//   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+//   fetch(url)
+//     .then(function(response) {
+//       //make sure request was successful
+//       if (response.status != 200) {
+//         return {
+//           text: "Error calling the Taste Dive API service: " + response.statusText
+//         }
+//       }
+//       return response.json();
+//     }).then(function(json) {
+//       console.log(json);
+//       // updateResult(json.text);
+//     });
+
+
+
+// document.getElementById("getRandom").addEventListener("click", function(event){
+//   event.preventDefault();
+//   const url2 = "https://www.themealdb.com/api/json/v1/1/random.php";
+//   fetch(url2)
+//     .then(function(response) {
+//       //make sure request was successful
+//       if (response.status != 200) {
+//         return {
+//           text: "Error calling the Taste Dive API service: " + response.statusText
+//         }
+//       }
+//       return response.json();
+//     }).then(function(json) {
+//       console.log(json);
+//       let results;
+//       // updateResult(json.text);
+//     });
+// })
+document.getElementById("categorySubmit").addEventListener("click", function(event){
   event.preventDefault();
-  const value = document.getElementById("input").value;
-
-  //check if field is empty
-  if (value === "") {
-    return;
-  }
-  console.log(value);
-
-  const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
-  fetch(url)
-    .then(function(response) {
-      //make sure request was successful
-      if (response.status != 200) {
-        return {
-          text: "Error calling the Taste Dive API service: " + response.statusText
-        }
-      }
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      // updateResult(json.text);
-    });
-});
-
-
-
-document.getElementById("getRandom").addEventListener("click", function(event){
-  event.preventDefault();
-  const url2 = "https://www.themealdb.com/api/json/v1/1/random.php";
-  fetch(url2)
-    .then(function(response) {
-      //make sure request was successful
-      if (response.status != 200) {
-        return {
-          text: "Error calling the Taste Dive API service: " + response.statusText
-        }
-      }
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      let results;
-      // updateResult(json.text);
-    });
-})
-document.getElementById("getArea").addEventListener("click", function(event){
-  event.preventDefault();
-  const url3 = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + country;
+  const category = document.getElementById("categoryInput").value;
+  const url3 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + category;
   fetch(url3)
     .then(function(response) {
       //make sure request was successful
       if (response.status != 200) {
         return {
-          text: "Error calling the Taste Dive API service: " + response.statusText
+          text: "Error calling the MealDB API service: " + response.statusText
         }
       }
       return response.json();
     }).then(function(json) {
       console.log(json);
       let results;
-      // updateResult(json.text);
+      results += "<h1> " + category + "Meals </h1>";
+      results += "<section class = 'meal-grid'>"
+      
+      for(let i = 0; i < json.meals.length; i++) {
+        results += "<div class = 'mealResults'>";
+        results += "<p> " + json.meals[i].strMeal + "</p>";
+        results += "<img src=" + json.meals[i].strMealThumb + " width = 200px>";
+        results += "</div>";
+      }
+      results += "</section>"
+      
+      
+      updateResult(results);
     });
 })
 
+
+
 function updateResult(info) {
-  document.getElementById('results').textContent = info;
+  document.getElementById('results').innerHTML = info;
 }
