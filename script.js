677962ca25f6179ee556806d760d730be53cf183
Justin Hill <jrhill426@gmail.com>
2021-02-27 11:11:32 -0700

    Finished Product?

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-document.getElementById("getRandom").addEventListener("click", function(event){
-  event.preventDefault();
-  const url = "https://www.themealdb.com/api/json/v1/1/random.php";
-  fetch(url)
-    .then(function(response) {
-      //make sure request was successful
-      if (response.status != 200) {
-        return {
-          text: "Error calling the MealDB API service: " + response.statusText
-        }
-      }
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      let randoms = "";
-      randoms +="<div class='random'>";
-      randoms += "<h2 class='enjoy'>" + "Enjoy Your Meal!" + "</h2>";
-      for (let i=0; i < json.meals.length; i++) {
-        randoms += "<h2 class='mealName'>" + json.meals[i].strMeal + "</h2>"
-        randoms += "<h3 class='area'>" + json.meals[i].strArea + "  |  " + json.meals[i].strCategory + "</h3>"
-        randoms += "<img class='image' src='" + json.meals[i].strMealThumb + "'>"
-        randoms += "<p>Instructions: " + json.meals[i].strInstructions + "</p>"
-      }
-      randoms += "</div>"
-      document.getElementById("randomResults").innerHTML = randoms;
-      updateResult(json.text);
-    });
+document.getElementById("getRandom").addEventListener("click", function(event){	
+  event.preventDefault();	
+  const url = "https://www.themealdb.com/api/json/v1/1/random.php";	
+  fetch(url)	
+    .then(function(response) {	
+      //make sure request was successful	
+      if (response.status != 200) {	
+        return {	
+          text: "Error calling the MealDB API service: " + response.statusText	
+        }	
+      }	
+      return response.json();	
+    }).then(function(json) {		
+      let randoms = "";	
+      randoms +="<div class='random'>";	
+      randoms += "<h2 class='enjoy'>" + "Enjoy Your Meal!" + "</h2>";	
+      for (let i=0; i < json.meals.length; i++) {	
+        randoms += "<h2 class='mealName'>" + json.meals[i].strMeal + "</h2>"	
+        randoms += "<h3 class='area'>" + json.meals[i].strArea + "  |  " + json.meals[i].strCategory + "</h3>"	
+        randoms += "<img class='image' src='" + json.meals[i].strMealThumb + "'>"	
+        randoms += "<div class='instructions'>"	
+        randoms += "<ol>" + "<li>"	
+          for(let j = 0; j < json.meals[i].strInstructions.length-1; j++) {	
+            if (json.meals[i].strInstructions.charAt(j) === ".") {	
+              randoms += "</li>" + "<li>"	
+            }	
+            else {	
+              randoms += json.meals[i].strInstructions[j]	
+            }	
+          }	
+        randoms +="</ol>"	
+        randoms += "</div>"	
+      }	
+      randoms += "</div>"	
+      document.getElementById("results").innerHTML = "";
+      document.getElementById("randomResults").innerHTML = randoms;		
+    });	
 })
 
 document.getElementById("categorySubmit").addEventListener("click", function(event){
@@ -41,25 +51,22 @@ document.getElementById("categorySubmit").addEventListener("click", function(eve
       }
       return response.json();
     }).then(function(json) {
-      console.log(json);
-      let results;
-      results += "<h1> " + category + "Meals </h1>";
+      let results = "<h1> " + category.charAt(0).toUpperCase() + category.slice(1,category.length) + " Meals </h1>";
       results += "<section class = 'meal-grid'>"
-
       for(let i = 0; i < json.meals.length; i++) {
         results += "<div class = 'mealResults'>";
-        results += "<p> " + json.meals[i].strMeal + "</p>";
+        results += "<h4> <strong>" + json.meals[i].strMeal + "</strong> </h4>";
         results += "<img src=" + json.meals[i].strMealThumb + " width = 200px>";
         results += "</div>";
       }
       results += "</section>"
-
       updateResult(results);
     });
 })
+
 document.getElementById("mealLetterSubmit").addEventListener("click", function(event){
   event.preventDefault();
-  const value = document.getElementById("mealLEtterInput").value;
+  const value = document.getElementById("mealLetterInput").value;
   if (value === "") {
     return;
   }
@@ -74,14 +81,13 @@ document.getElementById("mealLetterSubmit").addEventListener("click", function(e
       }
       return response.json();
     }).then(function(json) {
-      console.log(json);
-      let results;
-      results += "<h1> Meals from letter " + value.charAt(0) + " </h1>";
+      let results = "<h1> Meals from letter " + value.charAt(0).toUpperCase() + " </h1>";
       results += "<section class = 'meal-grid'>"
       for(let i = 0; i < json.meals.length; i++) {
         results += "<div class = 'mealResults'>";
-        results += "<p> " + json.meals[i].strMeal + "</p>";
-        results += "<p> " + json.meals[i].strArea + "</p>";
+        results += "<h4> <strong>" + json.meals[i].strMeal + "</strong> </h4>";
+        results += "<p> Area: <em>" + json.meals[i].strArea + "</em> </p>";
+        results += "<p> Category: <em>" + json.meals[i].strCategory + "</em> </p>";
         results += "<img src=" + json.meals[i].strMealThumb + " width = 200px>";
         results += "</div>";
       }
@@ -91,5 +97,11 @@ document.getElementById("mealLetterSubmit").addEventListener("click", function(e
 })
 
 function updateResult(info) {
+  document.getElementById('randomResults').innerHTML = "";
+  document.getElementById('results').innerHTML = "";
   document.getElementById('results').innerHTML = info;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
